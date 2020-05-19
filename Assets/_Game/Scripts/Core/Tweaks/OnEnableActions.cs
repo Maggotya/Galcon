edccaf3673e05817e1916182fe726e0fbe8f122e
Anton Kerp <anton_kerp@mail.ru>
2020-05-20 00:58:47 +0700

    Fix missing script

---
+++ Assets/_Game/Scripts/Core/Tweaks/OnEnableActions.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Core.Tweaks
+{
+    public class OnEnableActions : MonoBehaviour
+    {
+        [SerializeField] public UnityEvent onEnableEvent;
+
+        ////////////////////////////////////////////////
+
+        private void OnEnable()
+            => onEnableEvent?.Invoke();
+    }
+}
